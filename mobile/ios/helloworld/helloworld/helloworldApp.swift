53d1bd61754f77362226f9819e340699aad3d5c2
zkjet <zkjet@mailfence.com>
2023-04-21 09:46:34 -0600

    added ios and swift, added ios .gitignore items

---
+++ mobile/ios/helloworld/helloworld/helloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloworldApp.swift
+//  helloworld
+//
+//  Created by zkjet on 4/21/23.
+//
+
+import SwiftUI
+
+@main
+struct helloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
