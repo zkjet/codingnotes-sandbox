79a830c1e4e948d9a3096953f93407940c1d4ce3
zkjet <zkjet@mailfence.com>
2023-04-20 22:58:51 -0600

    added feathersjs chat

---
+++ backend/featherjs/feathers-chat/lib/services/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.services = void 0;
+const services = (app) => {
+    // All services will be registered here
+};
+exports.services = services;
