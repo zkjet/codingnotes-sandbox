64b914e8eab5d908fc71ccba9a2167b0e7a4ab65
zkjet <zkjet@mailfence.com>
2023-04-24 21:36:28 -0600

    replaced large sized music clips with smaller sound fx, renamed dirs

--- language/python/automaterandommusic.py
+++ language/python/automaterandommusic.py
@@ -3,14 +3,14 @@ import os
 import playsound
 
 
-def get_random_song():
-    music_dir = './tiesto_songs'
+def get_random_soundfx():
+    music_dir = './sound_effects'
     songs = os.listdir(music_dir)
     song = random.choice(songs)
     output = os.path.join(song)
     newoutput = str(output.rstrip())
     print(newoutput)
-    playsound.playsound("./tiesto_songs/"+newoutput)
+    playsound.playsound("./sound_effects/"+newoutput)
 
 
-get_random_song()
+get_random_soundfx()
